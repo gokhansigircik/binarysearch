c57ced8b87d0426aaf6207eb975dd89d6c2e1510
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-02-15 12:09:25 -0500

    new day new algo

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -139,6 +139,66 @@ class BinarySearchTree {
 
     this.print(node.left, spaceCnt);
   }
+// *****************WEDNESDAY*************************************
+ /**
+ * Determines if this tree contains the given searchVal.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} searchVal The number to search for in the node's data.
+ * @returns {boolean} Indicates if the searchVal was found.
+ */
+ contains(searchVal) {
+  if (this.isEmpty()) return false;
+
+  let runner = this.root;
+  while (runner) {
+    if ( runner.data == searchVal){
+      return true;
+    }
+    if (runner.data > searchVal) {
+      runner = runner.left;
+      // console.log(runner.data)
+    } else if (runner.data < searchVal) {
+      runner = runner.right;
+      // console.log(runner.data)
+    }
+  }
+  return false;
+}
+
+ /**
+  * Determines if this tree contains the given searchVal.
+  * - Time: O(?).
+  * - Space: O(?).
+  * @param {number} searchVal The number to search for in the node's data.
+  * @returns {boolean} Indicates if the searchVal was found.
+  */
+ containsRecursive(searchVal, current = this.root) {
+  if (this.isEmpty()) return false;
+  if (current === null) return false;
+  if (current.data === searchVal){
+    return true;
+  }
+  if (current.data > searchVal) {
+    return this.containsRecursive(searchVal, current.left);
+  } else if (current.data < searchVal) {
+    return this.containsRecursive(searchVal, current.right);
+  }
+}
+ /**
+  * Calculates the range (max - min) from the given startNode.
+  * - Time: O(?).
+  * - Space: O(?).
+  * @param {Node} startNode The node to start from to calculate the range.
+  * @returns {number|null} The range of this tree or a sub tree depending on if the
+  *    startNode is the root or not.
+  */
+ range(startNode = this.root) {
+  let max = this.max();
+  let min = this.min();
+  if (this.isEmpty()) return false;
+  return (max - min);
+
 }
 
 // const emptyTree = new BinarySearchTree();
@@ -155,7 +215,7 @@ const twoLevelTree = new BinarySearchTree();
 twoLevelTree.root = new BSTNode(10);
 twoLevelTree.root.left = new BSTNode(5);
 twoLevelTree.root.right = new BSTNode(15);
-console.log(twoLevelTree.root.min());
+// console.log(twoLevelTree.root.min());
 
 /* threeLevelTree 
         root
@@ -171,10 +231,14 @@ threeLevelTree.root.left = new BSTNode(5);
 threeLevelTree.root.left.left = new BSTNode(2);
 threeLevelTree.root.left.right = new BSTNode(6);
 threeLevelTree.root.right = new BSTNode(15);
-threeLevelTree.root.right.left = new BSTNode(13);
-console.log(threeLevelTree.minRecursive());
-console.log(threeLevelTree.max());
-console.log(threeLevelTree.maxRecursive());
+// threeLevelTree.root.right.left = new BSTNode(13);
+// console.log(threeLevelTree.minRecursive());
+// console.log(threeLevelTree.max());
+// console.log(threeLevelTree.maxRecursive());
+
+// console.log(threeLevelTree.contains(15))
+// console.log(threeLevelTree.contains(42))
+console.log(threeLevelTree.range());
 
 /* fullTree
                     root
@@ -206,45 +270,3 @@ console.log(threeLevelTree.maxRecursive());
 //   .insert(90);
 
 
-
-// isEmpty() {
-//   if(this.root === null){
-//       return true;
-//   }else return ;
-// }
-// min(current = this.root) {
-//   if(this.isEmpty()){
-//       return null
-//   }
-//   while(current.left){
-//       current = current.left
-//   }
-//   return current.data
-// }
-// minRecursive(current = this.root) {
-//   if(current === null){
-//       return null
-//   }
-//   if(current.left === null){
-//       return current.data
-//   }
-//   return this.minRecursive(current.left)
-// }
-// max(current = this.root) {
-//   if(this.isEmpty()){
-//       return null
-//   }
-//   while(current.right){
-//       current = current.right
-//   }
-//   return current.data
-// }
-// maxRecursive(current = this.root) {
-//   if(current === null){
-//       return null
-//   }
-//   if(current.right === null){
-//       return current.data
-//   }
-//   return this.maxRecursive(current.right)
-// }
